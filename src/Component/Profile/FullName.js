e121a576139b70add327c667b3879e87ab8a0fbb
Ghaythbelhadjyounes <Ghaythbelhadjyounes@gmail.com>
2022-05-11 16:04:18 +0100

    react component

---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,5 @@
+import React from "react";
+const FullName = () => {
+  return <h1>Ghayth Bel Hadj Younes</h1>;
+};
+export default FullName;
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,6 @@
+import React from "react";
+import photo from "../../ghayth.jpg"
+const ProfilePhoto = () => {
+  return <img src={photo} alt="ghayth"/>;
+};
+export default ProfilePhoto;
